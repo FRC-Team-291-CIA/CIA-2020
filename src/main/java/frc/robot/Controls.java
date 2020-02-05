d5366498e410b388e75be5a9b2c567969eb308dd
Christopher McCauliff Hess <Chris.Personal13@gmail.com>
2020-02-05 18:22:15 -0500

    Added Controls

--- src/main/java/frc/robot/Controls.java
+++ src/main/java/frc/robot/Controls.java
@@ -1,7 +1,16 @@
 package frc.robot;
 
 public class Controls {
-    
-    //TODO - Add buttons and joysticks to here for each controller and import them.
+    //This class contains all of the buttons and axis assignments
 
+    //Driver - Must be in BLANK mode
+        public static final int yAxis = 0;
+        public static final int xAxis = 1;
+        public static final int dumpOpenButton = 1;
+        public static final int shifterButton = 2;
+        public static final int driveOverrideButton = 3;
+        public static final int cameraSwitchButton = 4;
+
+    //Operator - Must be in BLANK mode
+        public static final int ballIntakeButton = 1;
 }
