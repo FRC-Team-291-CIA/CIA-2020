419c684c300f641295bd4bbb0a24b1f4178cf839
Christopher McCauliff Hess <Chris.Personal13@gmail.com>
2020-02-15 16:07:24 -0500

    Added Auto Structure

---
+++ src/main/java/frc/autonomous/autoMode.java
@@ -0,0 +1,18 @@
+package frc.autonomous;
+
+//TODO - Create different auto modes and selector
+public class autoMode{
+
+    public autoMode(){
+
+    }
+
+    public void start(){
+        this.select();
+    }
+
+    private void select(){
+
+    }
+
+}
