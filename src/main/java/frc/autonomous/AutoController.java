1b73f6373fa1558cda33dfa652bd1597797547d3
likhithborela <borela001@gannon.edu>
2020-03-09 18:28:35 -0400

    aut controller and automode abstract class added

---
+++ src/main/java/frc/autonomous/AutoController.java
@@ -0,0 +1,50 @@
+package frc.autonomous;
+
+import java.util.HashMap;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+
+public class AutoController {
+    // Strings here are for labeling our starting position on the field in the
+    // smartdashboard
+    private static final String left = "Left";
+    private static final String right = "Right";
+    private static final String center = "Center";
+
+    // Strings here are for determining which autonomous mode we want which will
+    // also be selectable through the smartdashboard
+    private static final String doNothing = "Do Nothing";//
+    private static final String crossBaseline = "Cross Baseline";// makes the robot cross the baseline, nothing else
+    private static final String dumpBallsOnce = "Dump Balls Once";// makes the robot go to the powerport to dump balls
+    private static final String dumpBallsAndPickUp = "Dump Balls And Pick Up More";// dumpBallsOnce plus some more
+    /*
+     * using sendable choosers for reference,
+     * https://first.wpi.edu/FRC/roborio/beta/docs/java/edu/wpi/first/wpilibj/
+     * smartdashboard/SendableChooser.html, Sendable choosers will let us determine
+     * which position we are in on the field and also which auto mode we want to do
+     */
+    private SendableChooser<String> fieldPosition = new SendableChooser<>();
+    private SendableChooser<String> autoChooser = new SendableChooser<>();
+    // hashmaps will be used to identify automodes and run them
+    private HashMap<String, Object> autoMap = new HashMap<String, Object>();
+
+    
+
+    public AutoController() {
+        fieldPosition.setDefaultOption("Left", left);
+        fieldPosition.addOption("Right", right);
+        fieldPosition.addOption("Center", center);
+
+        autoChooser.setDefaultOption("Do Nothing", doNothing);
+        autoChooser.addOption("Cross Baseline", crossBaseline);
+        autoChooser.addOption("Dump Balls Once", dumpBallsOnce);
+    }
+
+    public void start() {
+        select();
+    }
+
+    private void select() {
+
+    }
+
+}
