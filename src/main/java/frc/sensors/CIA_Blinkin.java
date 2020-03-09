3912cf3d0b1931af113f1ee857c4cd4db8b4769b
Christopher McCauliff Hess <Chris.Personal13@gmail.com>
2020-03-09 18:53:38 -0400

    Added Colors

--- src/main/java/frc/sensors/CIA_Blinkin.java
+++ src/main/java/frc/sensors/CIA_Blinkin.java
@@ -5,13 +5,20 @@ import edu.wpi.first.wpilibj.Spark;
 //TODO - Set Light Ports
 public class CIA_Blinkin{
     Spark lights;
+    double colorRed = 0.61;
+    double colorYellow = 0.69;
+    double colorOrange = 0.65;
+    double colorGreen = 0.77;
+    double colorWhite = 0.93;
+    double colorBlue = 0.87;
+    double colorHotPink = 0.57;
 
     public CIA_Blinkin(int pWMPort){ 
         lights = new Spark(pWMPort);
     }
 
     public void update(){
- 
+        lights.set(colorHotPink);
     }
 }
 
